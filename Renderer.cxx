cc0d1a871f79b9b7c87f184f740f45de47890783
Liquid Snake <m202-tracer@hotmail.co.uk>
2020-08-01 21:51:50 +0100

    Spent a lot of time getting glfw3 to link. Still getting errors

--- Renderer.cxx
+++ Renderer.cxx
@@ -1,7 +1,7 @@
 #include "Renderer.h"
 
-//#define GLFW_INCLUDE_VULKAN
-//#include <GLFW/glfw3.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -13,10 +13,10 @@
 #include <iostream>
 
 void Renderer::render() {
-    //glfwInit();
+    glfwInit();
 
-    //glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    //GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
@@ -27,11 +27,11 @@ void Renderer::render() {
     glm::vec4 vec;
     auto test = matrix * vec;
 
-    /*while(!glfwWindowShouldClose(window)) {
+    while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
 
-    glfwTerminate();*/
+    glfwTerminate();
 }
