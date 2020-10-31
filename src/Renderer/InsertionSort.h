7b57b07c27f133a19f67544c3f672eceb57a749c
Liquid Snake <m202-tracer@hotmail.co.uk>
2020-10-31 17:23:00 +0000

    Started work on insertion sort boiler plate

--- src/Renderer/InsertionSort.h
+++ src/Renderer/InsertionSort.h
@@ -5,6 +5,35 @@
 
 namespace InsertionSort {
 
+    struct ValueAndIndex {
+        float value;
+        uint32_t index;
+    };
+
+    struct Info {
+        uint32_t wasSwapped;
+        uint32_t dataSize;
+    };
+
+    VkDescriptorSetLayout createDescriptorSetLayout(VkDevice logicalDevice);
+
+    VkDescriptorPool createDescriptorPool(VkDevice logicalDevice, size_t maxSets);
+
+    VkDescriptorSet createDescriptorSet(
+        VkDevice logicalDevice,
+        VkDescriptorSetLayout& descriptorSetLayout,
+        VkDescriptorPool& descriptorPool,
+        VkBuffer valueAndIndexBuffer,
+        VkBuffer infoBuffer,
+        size_t numberOfElements);
+
+    VkPipelineLayout createPipelineLayout(VkDevice logicalDevice, VkDescriptorSetLayout descriptorSetLayout);
+
+    VkPipeline createPipeline(
+        VkDevice logicalDevice,
+        VkShaderModule shaderModule,
+        VkDescriptorSetLayout descriptorSetLayout,
+        VkPipelineLayout pipelineLayout);
 }
 
 #endif
