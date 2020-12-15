c59b94bda26b626833d1f7e382dde7e9949ad04a
Liquid Snake <m202-tracer@hotmail.co.uk>
2020-12-15 21:59:20 +0000

    Started work on mapping Agent to max x coordinate

--- compile_glsl.bat
+++ compile_glsl.bat
@@ -3,3 +3,4 @@ C:/VulkanSDK/1.2.141.2/Bin32/glslc.exe src/GLSL/shader.frag -o src/GLSL/frag.spv
 C:/VulkanSDK/1.2.141.2/Bin32/glslc.exe src/GLSL/Simulation.comp -o src/GLSL/Simulation.spv
 C:/VulkanSDK/1.2.141.2/Bin32/glslc.exe src/GLSL/InsertionSort.comp -o src/GLSL/InsertionSort.spv
 C:/VulkanSDK/1.2.141.2/Bin32/glslc.exe src/GLSL/Reduce.comp -o src/GLSL/Reduce.spv
+C:/VulkanSDK/1.2.141.2/Bin32/glslc.exe src/GLSL/MapAgentToX.comp -o src/GLSL/MapAgentToX.spv
