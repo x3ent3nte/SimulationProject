f398a42807972675a8381fc17bc87af284f50206
Liquid Snake <m202-tracer@hotmail.co.uk>
2021-05-11 22:08:10 +0100

    Added string constants for viking room model and texture

--- src/Renderer/Constants.h
+++ src/Renderer/Constants.h
@@ -10,8 +10,12 @@ namespace Constants {
 
     const std::string kFreyjaModelPath = "models/freyja.obj";
     const std::string kFreyjaTexturePath = "textures/freyja_texture.png";
+
     const std::string kArwingModelPath = "models/arwing.obj";
     const std::string kArwingTexturePath = "textures/arwing_texture.png";
+
+    const std::string kVikingRoomModelPath = "models/viking_room.obj";
+    const std::string kVikingRoomTexturePath = "textures/viking_room.png";
 
     const std::vector<const char*> kValidationLayers = {
         "VK_LAYER_KHRONOS_validation"
@@ -21,7 +25,7 @@ namespace Constants {
         VK_KHR_SWAPCHAIN_EXTENSION_NAME
     };
 
-    const bool kEnableValidationLayers = true;
+    constexpr bool kEnableValidationLayers = true;
 }
 
 #endif
