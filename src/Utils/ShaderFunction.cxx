1a92dbd6ecf19268452d12cdab81f7fd28476a81
Liquid Snake <m202-tracer@hotmail.co.uk>
2021-06-01 20:07:59 +0100

    Wrote helper classes to reduce Shader boiler plate

---
+++ src/Utils/ShaderFunction.cxx
@@ -0,0 +1,50 @@
+#include <Utils/ShaderFunction.h>
+
+ShaderFunction::ShaderFunction(
+    VkDevice logicalDevice,
+    size_t numberOfBindings,
+    const std::string& shaderPath)
+    : m_logicalDevice(logicalDevice)
+    , m_numberOfBindings(numberOfBindings) {
+
+    m_descriptorSetLayout = Compute::createDescriptorSetLayout(m_logicalDevice, numberOfBindings);;
+    m_pipelineLayout = Compute::createPipelineLayout(m_logicalDevice, m_descriptorSetLayout);
+    m_pipeline = Compute::createPipeline(shaderPath, m_logicalDevice, m_pipelineLayout);
+}
+
+ShaderFunction::~ShaderFunction() {
+    vkDestroyDescriptorSetLayout(m_logicalDevice, m_descriptorSetLayout, nullptr);
+    vkDestroyPipelineLayout(m_logicalDevice, m_pipelineLayout, nullptr);
+    vkDestroyPipeline(m_logicalDevice, m_pipeline, nullptr);
+}
+
+
+ShaderPool::ShaderPool(
+    std::shared_ptr<ShaderFunction> shaderFn,
+    size_t maxSets)
+    : m_shaderFn(shaderFn) {
+
+    m_descriptorPool = Compute::createDescriptorPool(m_shaderFn->m_logicalDevice, m_shaderFn->m_numberOfBindings, maxSets);
+}
+
+ShaderPool::~ShaderPool() {
+    vkDestroyDescriptorPool(m_shaderFn->m_logicalDevice, m_descriptorPool, nullptr);
+}
+
+
+ShaderLambda::ShaderLambda(
+    std::shared_ptr<ShaderPool> shaderPool,
+    const std::vector<Compute::BufferAndSize>& bufferAndSizes)
+    : m_shaderPool(shaderPool) {
+
+     m_descriptorSet = Compute::createDescriptorSet(
+        m_shaderPool->m_shaderFn->m_logicalDevice,
+        m_shaderPool->m_shaderFn->m_descriptorSetLayout,
+        m_shaderPool->m_descriptorPool,
+        bufferAndSizes);
+}
+
+ShaderLambda::~ShaderLambda() {
+
+}
+
---
+++ src/Utils/ShaderFunction.h
@@ -0,0 +1,70 @@
+#ifndef SHADER_FUNCTION_H
+#define SHADER_FUNCTION_H
+
+#include <Utils/Compute.h>
+
+#include <vulkan/vulkan.h>
+
+#include <memory>
+#include <string>
+
+class ShaderFunction {
+
+public:
+
+    ShaderFunction(
+        VkDevice logicalDevice,
+        size_t numberOfBindings,
+        const std::string& shaderPath);
+
+    virtual ~ShaderFunction();
+
+    VkDevice m_logicalDevice;
+
+    VkDescriptorSetLayout m_descriptorSetLayout;
+    VkPipelineLayout m_pipelineLayout;
+    VkPipeline m_pipeline;
+
+    const size_t m_numberOfBindings;
+
+private:
+
+};
+
+
+class ShaderPool {
+
+public:
+
+    ShaderPool(
+        std::shared_ptr<ShaderFunction> shaderFn,
+        size_t maxSets);
+
+    virtual ~ShaderPool();
+
+    std::shared_ptr<ShaderFunction> m_shaderFn;
+
+    VkDescriptorPool m_descriptorPool;
+
+private:
+
+};
+
+class ShaderLambda {
+
+public:
+
+    ShaderLambda(
+        std::shared_ptr<ShaderPool> shaderPool,
+        const std::vector<Compute::BufferAndSize>& bufferAndSizes);
+
+    virtual ~ShaderLambda();
+
+    std::shared_ptr<ShaderPool> m_shaderPool;
+    VkDescriptorSet m_descriptorSet;
+
+private:
+
+};
+
+#endif
